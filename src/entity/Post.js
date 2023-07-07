bc51b7a05fe1fc7de22a25c59827232dbb91fd1a
moncher8 <kookhotpink@gmail.com>
2023-07-07 16:31:42 +0900

    post entity update

--- src/entity/Post.js
+++ src/entity/Post.js
@@ -1,55 +1,55 @@
-import {EntitySchema} from 'typeorm';
+import { EntitySchema } from "typeorm";
 
 const postSchema = new EntitySchema({
-   name: 'post',
-   tableName: 'post',
-   columns: {
-      id: {
-         type: 'int',
-         primary: true,
-         generated: true,
-      },
-      title: {
-         type: 'varchar',
-         length: 255,
-      },
-      author: {
-         type: 'varchar',
-         length: 255,
-      },
-      //글 
-      content:{
-         type:'type',
-         nullable:false
-      },
-      //사진
-      pic:{
-         type: 'xml',
-         length:1000,
-         nullable:true
-      },
-      //작성시간
-      postdate:{
-         type: 'date',
-         nullable:false
-      },
-      //위치 태그
-      location:{
-         type: 'point',
-         foreign: true,
-      },
-      //좋아요
-      like:{
-         type:'int',
-         nullable:true
-      },
-      //답글
-      comment:{
-         type: 'varchar',
-         length:50,
-         nullable:true
-      }
-   },
+  name: "post",
+  tableName: "post",
+  columns: {
+    id: {
+      type: "int",
+      primary: true,
+      generated: true,
+    },
+    title: {
+      type: "varchar",
+      length: 255,
+    },
+    author: {
+      type: "varchar",
+      length: 255,
+    },
+    //글
+    content: {
+      type: "type",
+      nullable: false,
+    },
+    //사진
+    pic: {
+      type: "xml",
+      length: 1000,
+      nullable: true,
+    },
+    //작성시간
+    postdate: {
+      type: "date",
+      nullable: false,
+    },
+    //위치 태그
+    location: {
+      type: "point",
+      foreign: true,
+    },
+    //좋아요
+    like: {
+      type: "int",
+      nullable: true,
+    },
+    //답글
+    comment: {
+      type: "varchar",
+      length: 50,
+      nullable: true,
+    },
+  },
 });
 
 export default postSchema;
