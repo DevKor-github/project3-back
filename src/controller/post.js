f4e0a6c9c7a4b78955dfb5d0f984bd4fad47cdcd
eunbob <std50218@naver.com>
2023-07-07 16:55:11 +0900

    crud

--- src/controller/post.js
+++ src/controller/post.js
@@ -1,5 +1,29 @@
 import * as postService from '../service/postService.js';
 
+//Create Post
+export const createPost = async (req, res, next) => {
+	if(!req.body.name || !req.body.place || !req.body.description || !req.body.author) {
+		res.status(400).json({message: req.body.name});
+		return;
+	};
+
+	const post = {
+		name: req.body.name,
+		place: req.body.place,
+		description: req.body.description,
+		author: req.body.author,
+	};
+
+	try{
+		await postService.insertPost(post);
+		res.status(200).json({message: 'Successfully created'});
+	} catch(err){
+		next(err);
+	}
+
+};
+
+//Retrieve Post
 export const getPostList = async (req, res, next) => {
 	try {
 		const postList = await postService.getPostList();
@@ -8,3 +32,56 @@ export const getPostList = async (req, res, next) => {
 		next(err);
 	}
 };
+
+//Retrieve Post by condition
+export const getPostByCondition = async (req, res, next) => {
+	const id = req.body.id ? req.body.id : null;
+	const place = req.body.place ? req.body.place : null;
+	const author = req.body.author ? req.body.author : null;
+	const name = req.body.name ? req.body.name : null;
+
+	const condition = {
+		id: id,
+		place: place,
+		author: author,
+		name: name,
+	};
+
+	try{
+		const post = await postService.getPostByCondition(condition);
+		res.status(200).json(post);
+	}catch(err){
+		next(err);	
+	}
+};
+
+//Update Post
+export const updatePost = async (req, res, next) => {
+	const id = req.params.id;
+	const post = {
+		name: req.body.name,
+		place: req.body.place,
+		description: req.body.description,
+		author: req.body.author,
+	};
+
+	try{
+		await postService.updatePost(id, post);
+		res.status(200).json({message: 'Successfully updated'});
+	}catch(err){
+		next(err);
+	}
+};
+
+//Delete Post
+export const deletePost = async (req, res, next) => {
+	const id = req.params.id;
+
+	try{
+		await postService.deletePost(id);
+		res.status(200).json({message: 'Successfully deleted'});
+	}catch(err){
+		next(err);
+	}
+};
+
--- src/router/post.js
+++ src/router/post.js
@@ -3,6 +3,19 @@ import * as postController from '../controller/post.js';
 
 const router = express.Router();
 
+//Create Post
+router.post('/', postController.createPost);
+
+//Retrieve Post
 router.get('/', postController.getPostList);
+
+//Retrieve Post by condition
+router.get('/search', postController.getPostByCondition);
+
+//Update Post by ID
+router.put('/:id', postController.updatePost);
+
+//Delete Post by ID
+router.delete('/:id', postController.deletePost);
 
 export default router;
